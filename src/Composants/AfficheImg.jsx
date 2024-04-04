1b59c250a1cabe75af74cd0bb886ad09362e3ede
khalid21456 <khalidedaoudi8@gmail.com>
2024-04-04 06:39:15 +0100

    add a card

---
+++ src/Composants/AfficheImg.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+
+function AfficheImg(props) {
+    return(
+        <>
+            <img src={props.url}/>
+        </>
+     )
+} 
+
+export default AfficheImg
--- src/Composants/Compte.jsx
+++ src/Composants/Compte.jsx
@@ -1,36 +1,53 @@
-import ReactDOM from "react-dom";
-import React , {useEffect}  from "react";
-import "../index.css"
-import "../style/Compte.css"
+import React, { useEffect } from "react";
+import "../index.css";
+import "../style/Compte.css";
 import AfficherData from "./AfficheDate";
-function AfficherCompte() {    
-    
-    return (
-        <>
-        <div className="flex justify-center">
-            <div className="container bg-white shadow-md rounded-md mt-9">
-                <div className="flex justify-evenly pt-9">
-                    <div>
-                        <label className="pt-2 pr-4 font-bold">Name</label>
-                        <input type="text" name="name" className="name nice-input" placeholder="Enter your Name"/>
-                    </div>
-                    <div>
-                      <label className="pt-2 pr-4 font-bold">Number</label>
-                      <input type="text" name="numb" className="numb nice-input" placeholder="Enter your Number"/>
-                    </div>
-                </div>
-                <div className="flex justify-center pt-9">
-                    <button id="AjouterBtn" className="AjouterBtn hover:from-indigo-600 hover:to-purple-600 text-white font-bold py-2 px-4 rounded-full shadow-md hover:shadow-lg w-56">
-                        Ajouter
-                    </button>
-                </div>
+import AfficheImg from "./AfficheImg";
+
+function AfficherCompte() {
+  return (
+    <>
+      <div className="flex justify-center">
+        <div className="container bg-white shadow-md rounded-md mt-9">
+          <div className="flex justify-evenly pt-9">
+            <div>
+              <label className="pt-2 pr-4 font-bold">Name</label>
+              <input
+                type="text"
+                name="name"
+                className="name nice-input"
+                placeholder="Enter your Name"
+              />
             </div>
+            <div>
+              <label className="pt-2 pr-4 font-bold">Number</label>
+              <input
+                type="text"
+                name="numb"
+                className="numb nice-input"
+                placeholder="Enter your Number"
+              />
+            </div>
+          </div>
+          <div className="flex justify-center pt-9">
+            <button
+              id="AjouterBtn"
+              className="AjouterBtn hover:from-indigo-600 hover:to-purple-600 text-white font-bold py-2 px-4 rounded-full shadow-md hover:shadow-lg w-56"
+            >
+              Ajouter
+            </button>
+          </div>
         </div>
-        <div id="data">
-            <AfficherData/>
+      </div>
+      <div className="flex justify-evenly ml-14">
+        <div className="data">
+          <AfficherData />
         </div>
-        </>
-    )
+        <div className="data shadow-md rounded-md bg-white mt-9"></div>
+            <AfficheImg/>
+        </div>
+    </>
+  );
 }
 
-export default AfficherCompte
+export default AfficherCompte;
