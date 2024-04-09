c69618f93c2f4d8587e36fc34bc834d5cb39651c
khalid21456 <khalidedaoudi8@gmail.com>
2024-04-10 00:11:27 +0100

    events

--- src/Composants/Evenement.jsx
+++ src/Composants/Evenement.jsx
@@ -1,9 +1,15 @@
 import React from "react";
-
+import "../style/Event.css";
 
 export default function Evenement() {
-    return (
-        <h1>Onglet des evenements</h1>
-    )
+  return (
+    <div className="container1 flex justify-center scroll-m-14">
+      <div className="subContainer mt-5">
+        <div className="post border border-gray-400 shadow-sm rounded-sm bg-white"></div>
+        <div className="post border border-gray-400 shadow-sm rounded-sm bg-white"></div>
+        <div className="post border border-gray-400 shadow-sm rounded-sm bg-white"></div>
+        <div className="post border border-gray-400 shadow-sm rounded-sm bg-white"></div>
+      </div>
+    </div>
+  );
 }
-
